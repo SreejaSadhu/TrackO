6077b0f0b1029d309f9f6f91ab83660e2b598b59
SreejaSadhu <sreejasadhu06@gmail.com>
2025-03-18 23:25:30 +0530

    Added screenshots for README and updated SideMenu

--- frontend/expense-tracker/src/components/Layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/Layouts/SideMenu.jsx
@@ -6,7 +6,6 @@ import CharAvatar from "../Cards/CharAvatar";
 
 const SideMenu = ({ activeMenu }) => {
   const { user, clearUser } = useContext(UserContext);
-
   const navigate = useNavigate();
 
   const handleClick = (route) => {
@@ -14,7 +13,6 @@ const SideMenu = ({ activeMenu }) => {
       handelLogout();
       return;
     }
-
     navigate(route);
   };
 
@@ -29,13 +27,13 @@ const SideMenu = ({ activeMenu }) => {
       <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
         {user?.profileImageUrl ? (
           <img
-            src={user?.profileImageUrl || ""}
+            src={user.profileImageUrl}
             alt="Profile Image"
             className="w-20 h-20 bg-slate-400 rounded-full"
           />
         ) : (
           <CharAvatar
-            fullName={fullname}
+            fullName={user?.fullName || ""}
             width="w-20"
             height="h-20"
             style="text-xl"
@@ -43,7 +41,7 @@ const SideMenu = ({ activeMenu }) => {
         )}
 
         <h5 className="text-gray-950 font-medium leading-6">
-          {user.fullName || ""}
+          {user?.fullName || ""}
         </h5>
       </div>
 
@@ -51,7 +49,7 @@ const SideMenu = ({ activeMenu }) => {
         <button
           key={`menu_${index}`}
           className={`w-full flex items-center gap-4 text-[15px] ${
-            activeMenu == item.label ? "text-white bg-primary" : ""
+            activeMenu === item.label ? "text-white bg-primary" : ""
           } py-3 px-6 rounded-lg mb-3`}
           onClick={() => handleClick(item.path)}
         >
